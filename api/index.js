1591911cccbb3f4b8e9487903bd8f1a3e0aaadcc
SARTHAK ATRISH <sarthakatrish@SARTHAKs-MacBook-Air-2.local>
2026-02-16 23:50:25 +0530

    Fix serverless function configuration for Vercel

--- api/index.js
+++ api/index.js
@@ -1,4 +1,9 @@
 import { createRequestHandler } from "@react-router/node";
+
+// @ts-ignore
 import * as build from "../build/server/index.js";
 
-export default createRequestHandler({ build });
+export default createRequestHandler({
+  build,
+  mode: process.env.NODE_ENV,
+});
