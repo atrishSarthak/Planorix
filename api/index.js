62aba90ecc8ca394b89c9472ab0e29968ddf38bc
SARTHAK ATRISH <sarthakatrish@SARTHAKs-MacBook-Air-2.local>
2026-02-16 23:52:24 +0530

    Switch to SPA mode for simpler Vercel deployment

--- api/index.js
+++
@@ -1,9 +0,0 @@
-import { createRequestHandler } from "@react-router/node";
-
-// @ts-ignore
-import * as build from "../build/server/index.js";
-
-export default createRequestHandler({
-  build,
-  mode: process.env.NODE_ENV,
-});
